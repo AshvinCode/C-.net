358ad9415d64eb9313bd758ed0370decc5762828
ashvin.mori <ashvin.mori@binaryrepublik.com>
2024-09-26 19:47:05 -0700

    MS sql Connection, file

---
+++ TestCrud/Test ADO DBContext MS SQL/DatabaseConnection/DatabaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMC.Estimator.AdoDBContext.DatabaseConnection
+{
+    public class DatabaseConnection
+    {
+        public static string connectionString = string.Empty;
+    }
+}
---
+++ TestCrud/Test ADO DBContext MS SQL/DatabaseConnection/EstimatorDBContext.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMC.Estimator.AdoDBContext.DatabaseConnection
+{
+    public class EstimatorDBContext
+    {
+        private static readonly string? connectionString;
+        static EstimatorDBContext()
+        {
+            connectionString = DatabaseConnection.connectionString;
+        }
+        public static async Task<DataSet> GetDataAsync(string storedProcedureName, object[] parameters, object[] values)
+        {
+            DataSet dataSet = new DataSet();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(storedProcedureName, con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        await con.OpenAsync();
+
+                        // Using for loop to set parameter names and values for stored procedure
+                        for (int i = 0; i < parameters.Length; i++)
+                        {
+                            string parameter = Convert.ToString(parameters[i]);
+                            string value = Convert.ToString(values[i]);
+                            cmd.Parameters.AddWithValue(parameter, value);
+                        }
+
+                        using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd))
+                        {
+                            await Task.Run(() => sqlDataAdapter.Fill(dataSet));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return dataSet;
+        }
+
+        public static async Task<long?> InsertUpdateDataAsync(string spName, object[] parameters, object[] values, object[] sqlDbTypes)
+        {
+            long? id = null;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(Convert.ToString(spName), con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        await con.OpenAsync();
+
+                        // Using for loop to set parameter names and values for the stored procedure
+                        for (int i = 0; i < parameters.Length; i++)
+                        {
+                            string parameter = Convert.ToString(parameters[i]);
+                            string value = Convert.ToString(values[i]);
+                            SqlDbType sqlDbType = (SqlDbType)Enum.Parse(typeof(SqlDbType), sqlDbTypes[i].ToString());
+                            SqlParameter tvpParam = cmd.Parameters.AddWithValue(parameter, value);
+                            tvpParam.SqlDbType = sqlDbType;
+                        }
+
+                        object statusId = await cmd.ExecuteScalarAsync();
+                        if (statusId != null)
+                        {
+                            id = Convert.ToInt64(statusId);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return id;
+        }
+
+    }
+}
